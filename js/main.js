b137e30ba13a761396ed17c2909041c7d2299029
Michał Borzęcki <ziqzzaq.web@gmail.com>
2017-12-25 15:44:44 +0100

    First commit

---
+++ js/main.js
@@ -0,0 +1,28 @@
+setTimeout(showPage, 6000);
+function showPage() {
+    document.getElementById("loader_img").style.display = "none";
+    document.getElementById("loaded_content").style.display = "block";
+}
+setTimeout(secondAnim, 4000);
+function secondAnim() {
+    document.getElementById("loader_img").className = "o-loader-img js-sliding_out";
+}
+
+
+
+$(window).scroll(function() {
+    $(".js-items-to-slide").each(function(){
+        var pos = $(this).offset().top;
+        var winTop = $(window).scrollTop();
+        if (pos < winTop + 900) {
+            $(this).addClass("js-slideing");
+        }
+    });
+});
+$('.c-navbar_right_button--search').click( function() {
+    $('#search').toggleClass('js-slide_right');
+    $('#search').toggleClass('js-visible');
+});
+$('.o-navbar_menu_button').click(function() {
+    $('#menu').slideToggle(150);
+});
