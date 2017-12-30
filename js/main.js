0908b9b42446c92d856ddae9cf51e3a2047b2863
Michał Borzęcki <ziqzzaq.web@gmail.com>
2017-12-30 17:30:06 +0100

    Second commit
    Code improvement, refactoring

--- js/main.js
+++ js/main.js
@@ -1,17 +1,19 @@
 setTimeout(showPage, 6000);
+
 function showPage() {
     document.getElementById("loader_img").style.display = "none";
     document.getElementById("loaded_content").style.display = "block";
 }
+
 setTimeout(secondAnim, 4000);
+
 function secondAnim() {
     document.getElementById("loader_img").className = "o-loader-img js-sliding_out";
 }
 
 
-
-$(window).scroll(function() {
-    $(".js-items-to-slide").each(function(){
+$(window).scroll(function () {
+    $(".js-items-to-slide").each(function () {
         var pos = $(this).offset().top;
         var winTop = $(window).scrollTop();
         if (pos < winTop + 900) {
@@ -19,10 +21,10 @@ $(window).scroll(function() {
         }
     });
 });
-$('.c-navbar_right_button--search').click( function() {
+$('.c-navbar_right_button--search').click(function () {
     $('#search').toggleClass('js-slide_right');
     $('#search').toggleClass('js-visible');
 });
-$('.o-navbar_menu_button').click(function() {
+$('.o-navbar_menu_button').click(function () {
     $('#menu').slideToggle(150);
 });
